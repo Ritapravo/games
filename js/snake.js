b902761307f505c6fc4b6fb40c4312a369858f6c
ritapravo <ritopravosarker@gmail.com>
2021-10-27 21:28:03 +0530

    Wed 27 Oct 21:28:03 IST 2021

--- js/snake.js
+++ js/snake.js
@@ -1,7 +1,10 @@
 // Game Constants & Variables
 //https://www.pinclipart.com/pins/snake/
 
-let level = prompt("Enter the level from 1 to 20", "1");
+let level = 5;
+//level = prompt("Enter the level from 1 to 20", "5");
+
+
 console.log("level "+level+" selected");
 
 function displayLevel(){
@@ -10,14 +13,14 @@ function displayLevel(){
 }
 setInterval(displayLevel, 100);
 
-let inputDir = {x:0, y:0};
+let direction = {x:0, y:0};
 let speed = level*2;
-let lastPaintTime = 0;
+let last_update = 0;
 let score = 0;
 
 
 
-let snakeArr =[
+let snake =[
     {x:13, y:15}
 ]
 food = {x:6, y:7};
@@ -26,61 +29,62 @@ function displayScore(){
     //console.log();
     document.getElementById('score-val').innerHTML=score;
 }
-setInterval(displayScore, 100);
+setInterval(displayScore, speed);
 
 //
 function main(ctime){
     window.requestAnimationFrame(main);
-    if((ctime-lastPaintTime)/1000 < 1/speed){
+    if((ctime-last_update)/1000 < 1/speed){
         return;
     }
-    lastPaintTime = ctime;
+    last_update = ctime;
     //console.log(ctime);
     gameEngine();
+    //display_keys();
 }
 
-function isCollide(snake){
-    // If you bump into yourself
-    for (let i = 1; i < snakeArr.length; i++) {
-        const element = snakeArr[i];
-        if(snake[i].x===snake[0].x && snake[i].y === snake[0].y){
-            return true;
-        }
-    }
-    if(snake[0].x>=18 || snake[0].x<=0 || snake[0].y>=18 || snake[0].y<=0){
-        return true;
-    }
-    return false;
+
+function display_food() {
+    foodElement = document.createElement('div');
+    foodElement.style.gridRowStart = food.y;
+    foodElement.id = 'food_elem';
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.classList.add('food');
+    board.appendChild(foodElement);
+    document.getElementById('food_elem').style.animation = "animate 0.5s linear infinite";
 }
+
 
 function gameEngine(){
     // Part 1: Updating the snake array & food
-    if(isCollide(snakeArr)){
-        inputDir ={x:0, y:0};
-        alert("Game Over. Press any key to play again!");
-        snakeArr = [{x:13, y:15}];
+    if(Check()){
+        direction ={x:0, y:0};
+        alert("Game Over. Press any key to play again! Your Score: "+score);
+        snake = [{x:13, y:15}];
         score = 0;
     }
     // If you have eaten the food, increment the score and regenerate the food
-    if(snakeArr[0].y===food.y && snakeArr[0].x===food.x){
-        snakeArr.unshift({x:snakeArr[0].x+inputDir.x, y:snakeArr[0].y+inputDir.y});
+    if(snake[0].y===food.y && snake[0].x===food.x){
+        snake.unshift({x:snake[0].x+direction.x, y:snake[0].y+direction.y});
         let a = 2;
         let b = 16;
         score += 1;
         console.log(score);
-        food = {x: Math.round(a+(b-a)*Math.random()), y: Math.round(a+(b-a)*Math.random())};
+        let x_val = Math.round(a+(b-a)*Math.random());
+        let y_val = Math.round(a+(b-a)*Math.random());
+        food = {x: x_val, y: y_val};
     }
     // Moving the snake
-    for (let i = snakeArr.length-2; i>=0; i--) {
-        const element = snakeArr[i];
-        snakeArr[i+1] = {...snakeArr[i]};
+    for (let i = snake.length-2; i>=0; i--) {
+        const element = snake[i];
+        snake[i+1] = {...snake[i]};
     }
-    snakeArr[0].x += inputDir.x;
-    snakeArr[0].y += inputDir.y;
+    snake[0].x += direction.x;
+    snake[0].y += direction.y;
 
     // Part 2: Display the snake 
     board.innerHTML = "";
-    snakeArr.forEach((e, index)=>{
+    snake.forEach((e, index)=>{
         snakeElement = document.createElement('div');
         snakeElement.style.gridRowStart = e.y;
         snakeElement.style.gridColumnStart = e.x;
@@ -97,41 +101,126 @@ function gameEngine(){
     // Display the food
     foodElement = document.createElement('div');
     foodElement.style.gridRowStart = food.y;
+    foodElement.id = 'food_elem';
     foodElement.style.gridColumnStart = food.x;
     foodElement.classList.add('food');
     board.appendChild(foodElement);
+    document.getElementById('food_elem').style.animation = "animate 0.5s linear infinite";
 }
 
 
-
+//document.getElementById('food_elem').style.animation = "animate 1s linear infinite";
 
 window.requestAnimationFrame(main);
 
 window.addEventListener('keydown', e => {
-    inputDir = {x:0, y:1} //start the snake
+    direction = {x:0, y:1} //start the snake
     switch(e.key){
         case "ArrowUp":
-            console.log("ArrowUp");
-            inputDir.x = 0;
-            inputDir.y = -1;
+            //console.log("ArrowUp");
+            direction.x = 0;
+            direction.y = -1;
             break;
         case "ArrowDown":
-            console.log("ArrowDown");
-            inputDir.x = 0;
-            inputDir.y = 1;
+            //console.log("ArrowDown");
+            direction.x = 0;
+            direction.y = 1;
             break;
         case "ArrowLeft":
-            console.log("ArrowLeft");
-            inputDir.x = -1;
-            inputDir.y = 0;
+            //console.log("ArrowLeft");
+            direction.x = -1;
+            direction.y = 0;
             break;
         case "ArrowRight":
-            console.log("ArrowRight");
-            inputDir.x = 1;
-            inputDir.y = 0;
+            //console.log("ArrowRight");
+            direction.x = 1;
+            direction.y = 0;
             break;
         default:
             break;
         
     }
 });
+
+function Check(){
+    // If you bump into yourself
+    for (let i = 1; i < snake.length; i++) {
+        const element = snake[i];
+        if(snake[i].x===snake[0].x && snake[i].y === snake[0].y){
+            return true;
+        }
+    }
+    if(snake[0].x>=18 || snake[0].x<=0 || snake[0].y>=18 || snake[0].y<=0){
+        return true;
+    }
+    return false;
+}
+
+
+function display_keys() {
+    // FOR DISPLAYING THE EMPTY BOARD(NO NEED WHEN USING BACKGROUND IMAGE)
+
+   /*  boxElement1 = document.createElement('div');
+    boxElement1.style.gridRowStart = 1;
+    boxElement1.style.gridColumnStart = 1;
+    boxElement1.classList.add('item1');
+    keys.appendChild(boxElement1); */
+    
+
+    /* boxElement1 = document.createElement('div');
+    boxElement1.style.gridRowStart = 1;
+    boxElement1.style.gridColumnStart = 3;
+    boxElement1.classList.add('item1');
+    keys.appendChild(boxElement1);
+    boxElement1.id = "up_arrow"; */
+
+    // up arrow
+    boxElement1 = document.createElement('div');
+    boxElement1.style.gridRowStart = 1;
+    boxElement1.style.gridColumnStart = 2;
+    boxElement1.classList.add('item2');
+    keys.appendChild(boxElement1);
+    boxElement1.id = "up_arrow";
+    boxElement1.onclick=function(){
+        direction.x = 0;
+        direction.y = -1;
+    };
+    
+    // down arrow
+    boxElement2 = document.createElement('div');
+    boxElement2.style.gridRowStart = 2;
+    boxElement2.style.gridColumnStart = 2;
+    boxElement2.classList.add('item2');
+    keys.appendChild(boxElement2);
+    boxElement2.id = "down_arrow";
+    boxElement2.onclick=function(){
+        direction.x = 0;
+        direction.y = 1;
+    };
+
+    //left arrow
+    boxElement3 = document.createElement('div');
+    boxElement3.style.gridRowStart = 2;
+    boxElement3.style.gridColumnStart = 1;
+    boxElement3.classList.add('item2');
+    keys.appendChild(boxElement3);
+    boxElement3.id = "left_arrow";
+    boxElement3.onclick=function(){
+        direction.x = -1;
+        direction.y = 0;
+    };
+
+    //right arrow
+    boxElement4 = document.createElement('div');
+    boxElement4.style.gridRowStart = 2;
+    boxElement4.style.gridColumnStart = 3;
+    boxElement4.classList.add('item2');
+    keys.appendChild(boxElement4);
+    boxElement4.id = "right_arrow"; 
+    boxElement4.onclick=function(){
+        direction.x = 1;
+        direction.y = 0;
+    }; 
+
+}
+display_keys();
